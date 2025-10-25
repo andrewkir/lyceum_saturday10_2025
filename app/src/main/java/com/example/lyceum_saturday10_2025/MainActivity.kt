11e3ef0198018fb063222dc0af52c5ea53582df8
andrewkir <andrewkir2001@mail.ru>
2025-10-25 12:29:33 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lyceum_saturday10_2025/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.lyceum_saturday10_2025
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lyceum_saturday10_2025.examples.Profile
+import com.example.lyceum_saturday10_2025.examples.model.User
+import com.example.lyceum_saturday10_2025.ui.theme.Lyceum_saturday10_2025Theme
+import com.example.lyceum_saturday10_2025.ui.theme.Purple40
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Lyceum_saturday10_2025Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        Profile()
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Lyceum_saturday10_2025Theme {
+        Greeting("Android")
+    }
+}
